090001d79c484a5dbb264f4133fa6347a608dc38
W <git@worik.org>
2018-07-18 18:54:44 +1200

    Extensively modified to use ncurses better

--- gp/src/front_end.rs
+++ gp/src/front_end.rs
@@ -15,20 +15,66 @@ pub struct FrontEnd {
 
     // controller is in charge of the simulations
     controller:Controller,
+
+    main_window:WINDOW,
+    menu_window:WINDOW,
+    status_window:WINDOW,
 }
 
 impl FrontEnd {
-    pub fn new () -> FrontEnd {
+    pub fn new() -> FrontEnd {
 
         // The root directory of the process.  FIXME This should be
         // passed in on the command line, optionally
-        let root_dir = format!("{}/", env::current_dir().unwrap().to_str().unwrap().to_string());
+        let root_dir = format!("{}/",
+                               env::current_dir().unwrap().
+                               to_str().unwrap().
+                               to_string());
         
+        initscr();
+        raw();
+        start_color();
+        cbreak();
+        noecho();
+        curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+        keypad(stdscr(), true);
+        init_pair(1, COLOR_RED, COLOR_BLACK);
+        
+        /* Get the screen bounds. */
+        let (max_x, max_y, x, _y) = (COLS(), LINES(), 0, 0);
+
+        // Calculate where the menu, main and status panes are in
+        // terms of max_y.  y=0 is top of screen
+
+        // The number of lines the menu and status indows use.  This
+        // includes border FIXME make these cofigurable
+        let status_lines = 3;
+        let menu_lines = 3;
+
+        let (menu_h, main_h, status_h) =
+            (menu_lines, max_y - (menu_lines+status_lines), status_lines);
+        let (menu_y, main_y, status_y) =
+            (0, menu_lines, max_y - status_lines);
+        let menu_window = newwin(menu_h, max_x, menu_y, x);    
+        box_(menu_window, 0, 0);
+        let main_window = newwin(main_h, max_x, main_y, x);    
+        box_(main_window, 0, 0);
+        let status_window = newwin(status_h, max_x, status_y, x);    
+        box_(status_window, 0, 0);
+
+        redraw(main_window);
+        redraw(status_window);
+        redraw(menu_window);
         
         FrontEnd{
             //config:config,
             controller:Controller::new(root_dir.clone()),
             root_dir:root_dir,
+
+            
+            main_window:main_window,
+            menu_window:menu_window,
+            status_window:status_window,
         }
     }
     
@@ -50,22 +96,38 @@ impl FrontEnd {
         config
     }
     
-    fn do_project_menu(&mut self, win:WINDOW, name:&str) {
+    fn do_project(& mut self, name:&str) {
 
-        // For the project named in @param name, offer a menu of
-        // things to do with it
-
-        let status = self.controller.get_status(name);
-        // Get the root directory of the projects
-        let projects:Vec<_> = vec!["Create      ".to_string(), "Status".to_string()];
-        let actions:Vec<_> = projects.iter().zip(0..projects.len()).collect();
-        
-        status_line(&format!("Cleared: {} Runnning: {} Path: {}", status.cleared, status.running, status.path));        
         loop {
-            let c = make_menu(win, &actions, name);
+            werase(self.main_window);
+
+            let status = self.controller.get_status(name);
+            let projects:Vec<_> = vec!["Create", "Refresh Status"];        
+            // pub cleared:bool,
+            // pub running:bool,
+            // pub generation:usize,
+            // pub path:String, // FIXME This should be a reference
+            if mvwprintw(self.main_window, 1, 1, &format!("Name: {}", name)) != 0 {
+                panic!("Failed mvprint");
+            }
+            if mvwprintw(self.main_window, 2, 1, &format!("Cleared: {:?}", status.cleared)) != 0 {
+                panic!("Failed mvprint");
+            }
+            if mvwprintw(self.main_window, 3, 1, &format!("Running: {:?}", status.running)) != 0 {
+                panic!("Failed mvprint");
+            }
+            if mvwprintw(self.main_window, 4, 1, &format!("Generation: {:?}", status.generation)) != 0 {
+                panic!("Failed mvprint");
+            }
+            if mvwprintw(self.main_window, 5, 1, &format!("Path: {}", status.path)) != 0 {
+                panic!("Failed mvprint");
+            }
+            redraw(self.main_window);
+
+
+            let c = self.make_menu(&projects);
             // Got a key.
             
-            status_line(&format!("key: {}",c));
             match c {
                 // 0 allways sends us back 
                 0 => break,
@@ -81,73 +143,37 @@ impl FrontEnd {
 
                     // Use the controller o run the simulation.  If it
                     // fails to launch the status is set appropriately
-                    match self.controller.run_simulation(config.copy()) {
-                        Ok(_) => (), 
-                        Err(s) => status_line(&s.to_string()),
-                    }
+                    let status = match self.controller.run_simulation(config.copy()) {
+                        Ok(_) => "Ok".to_string(), 
+                        Err(s) => s.to_string(),
+                    };
+                    self.status_line(&status);
                 },
                 
-                2 => {
-                    self.do_project_status(win, name);
-                }
                 _ => (),
             }
+            
+            
         }
     }
 
 
-    fn do_project_status(& mut self, win:WINDOW, name:&str){
-        // Display the projects status
-
-        werase(win);
-
-        let status = self.controller.get_status(name);
-        // pub cleared:bool,
-        // pub running:bool,
-        // pub generation:usize,
-        // pub path:String, // FIXME This should be a reference
-        if mvwprintw(win, 1, 1, &format!("Name: {}", name)) != 0 {
-            panic!("Failed mvprint");
-        }
-        if mvwprintw(win, 2, 1, &format!("Cleared: {:?}", status.cleared)) != 0 {
-            panic!("Failed mvprint");
-        }
-        if mvwprintw(win, 3, 1, &format!("Running: {:?}", status.running)) != 0 {
-            panic!("Failed mvprint");
-        }
-        if mvwprintw(win, 4, 1, &format!("Generation: {:?}", status.generation)) != 0 {
-            panic!("Failed mvprint");
-        }
-        if mvwprintw(win, 5, 1, &format!("Path: {}", status.path)) != 0 {
-            panic!("Failed mvprint");
-        }
-        redraw(win);
-
-        // FIXME  This should be a menu offering refresh or back
-        wgetch(win);
-    }
-    
-    fn edit_config(&self, win:WINDOW, config:&Config) -> Config {
-        // This is waiting on developing technology to edit data 
-        werase(win);
+    // fn edit_config(&self, config:&Config) -> Config {
+    //     // This is waiting on developing technology to edit data 
         
-        config.copy()
-    }
-    
-    fn do_choose_project(&mut self, win:WINDOW) {
+    // }
 
+    fn do_choose_object(&mut self){
         // Get the root directory of the projects
         let proj_dir = Path::new("./Data/");
-        status_line(&format!("Current Directory {:?}", env::current_dir()));
+        self.status_line(&format!("Current Directory {:?}", env::current_dir()));
 
         // Get the sub-directories that are projects
         let mut projects:Vec<String> = Vec::new();
         let entries = match fs::read_dir(proj_dir) {
             Ok(v) => v,
             Err(e) => {
-                redraw(win);
                 let s = format!("Failed reading {:?} e: {} cd: {:?}", proj_dir.to_str(), e, env::current_dir());
-                display_model(win, &s);
                 fe_shut();
                 panic!(s);
             }
@@ -163,114 +189,99 @@ impl FrontEnd {
         }
 
         // Display the projects and wait for user to select one
-        let menu_vec:Vec<_> = projects.iter().zip(0..projects.len()).collect();
+        let menu_vec:Vec<_> = projects.iter().zip(1..(1+projects.len())).map(|x| format!("{} {}", x.0, x.1)).collect();
         
         loop {
-            let c = make_menu(win, &menu_vec, "TITLE");
+            self.fe_main(&menu_vec);
+            let c = self.make_menu(&vec!["Enter choice", "Display Config", "Quit"] );
             if c == 0 {
                 break;
             }else if c <= menu_vec.len() {
-                let project = menu_vec.iter().nth(c-1).unwrap().0;
-                status_line(&format!("Choose index {} project {}", c-1, &project));
-                self.do_project_menu(win, project);
+                let project = menu_vec.iter().nth(c-1).unwrap().clone();
+                self.status_line(&format!("Choose index {} project {}", c-1, &project));
+                self.do_project(&project);
             }else if c == 0{
                 break;
             }else{
-                status_line(&format!("Option {} not valid", c));
+                self.status_line(&format!("Option {} not valid", c));
             }
         }    
+        
     }
 
-    
-    fn fe_menu(&mut self) -> bool{
 
-        // Draw the main menu.  Return false if quit is choosen
-
-        /* Get the screen bounds. */
-        let (max_x, max_y, x, y) = main_window_dims();
-
-        // Make window FIXME do I have to?
-        let win = newwin(max_y, max_x, y, x);    
-        redraw(win);
-        
-        // Top left of main menu
-        let start_x = max_x/10 as i32;
-        let start_y = max_y/10 as i32;
-        
-        let msg = "Enter choice:";
-        if mvwprintw(win, start_y, start_x, &msg) != 0 {
-            panic!("Failed mvprint");
+    fn fe_main(&self, list:&Vec<String>) {
+        // Display a list in the main pane
+        let x = 1;
+        let mut y = 1;
+        werase(self.main_window);
+        for s in list.iter() {
+            mvwprintw(self.main_window, y, x, &*s);
+            y = y + 1;
         }
-
-        // The main menu
-        let menu_items  = vec!(("Choose Project", 1), ("Display Config", 2), ("Quit", 0));
-        for i in 0..menu_items.len() {
-            let item = format!("{} {}", i, menu_items.iter().nth(i).unwrap().0);
-            mvwprintw(win, start_y+i as i32 + 1 as i32, start_x, &item);
-        }
-
-        status_line(&"Waiting for menu choice".to_string());
-        let c = wgetch(win) - 48;
-
-        let mut ret = true;
-        match c {
-            0 => self.do_choose_project(win),
-            1 => self.display_config(win, "<PROJECT>"),
-            2 => ret = false,
-            _ => status_line(&format!("Menu choice {}", c).to_string()),
-        };
-        destroy_win(win);
-        ret
+        redraw(self.main_window);
     }
+
     pub fn fe_start(&mut self) {
-
-        initscr();
-        raw();
-        start_color();
-        cbreak();
-        noecho();
-        curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
-        keypad(stdscr(), true);
-        init_pair(1, COLOR_RED, COLOR_BLACK);
-        
-        
         loop {
-            if self.fe_menu() == false {
-                break;
+            let c = self.make_menu(&vec!["Choose Object", "Display Config"] );
+            match c {
+                0 => break,
+                1 => self.do_choose_object(),
+                2 => self.do_display_config("<PROJECT NAME>"),
+                _ => panic!(),
             }
         }
         fe_shut();
     }
     
-    fn display_config(& mut self, win:WINDOW, name:&str)  {
+    fn do_display_config(&self, name:&str)  {
 
         let config = self.default_config(name);
 
         let mut keys = config.data.keys();
-        eprintln!("display_config 4");
-
-        let (max_x, max_y) = (getmaxx(win), getmaxy(win));
-        // Top left of main menu
-        let start_x = max_x/10 as i32;
-        let start_y = max_y/10 as i32;
-        werase(win);
-
-        for y in 1..config.data.len() {
+        let mut cfg_strings:Vec<String> = Vec::new();
+        for _ in 1..config.data.len() {
             if let Some(key) = keys.next() {
                 let v = config.data.get(key).unwrap();
                 let item = format!("{}:\t{}", key, v);
-                eprintln!("y: {} x: {} max_x {}  max_y {} item {}", start_y+(1+y) as i32, start_x, max_x, max_y, item);
-                if mvwprintw(win, start_y+1+y as i32, start_x as i32, &item) != 0 {
-                    panic!(format!("Failed mvprint: y: {} x: {} max_x {}  max_y {}", start_y+(1+y) as i32, start_x, max_x, max_y));
-                }
-                redraw(win);
+                cfg_strings.push(item);
             }
         }
-        redraw(win);
-        // FIXME  Create a menu here!  0 => to go back
-        wgetch(win);
-        eprintln!("display_config 5");
+        self.fe_main(&cfg_strings);
     }
+
+    fn make_menu(&self, menu_items:&Vec<&str>) -> usize {
+        werase(self.menu_window);
+        //redraw(self.status_window);
+        // eprintln!("make_menu");
+        let mut x = 1; // Start of menu
+        // werase(self.menu_window);
+        for i in 1..menu_items.len()+1 {
+            let s = menu_items.iter().nth(i-1).unwrap();
+            if mvwprintw(self.menu_window, 1, x, &format!("{} {}", i, &s)) != 0{
+                panic!("Failed mvwprint");
+            }
+            eprintln!("make_menu {}", format!("{} {}", x, s));
+            x = x + s.len() as i32 + 3;
+        }
+        if mvwprintw(self.menu_window, 1, x, &format!("{} {}", 0, "Back")) != 0{
+            panic!("Failed mvwprint");
+        }
+        redraw(self.menu_window);
+        let ret = wgetch(self.menu_window) as usize - 48;
+        eprintln!("make_menu Got {} ", ret);
+        ret
+    }
+
+    fn status_line(& self, msg:&str) {
+        werase(self.status_window);
+        if mvwprintw(self.status_window, 1, 1, msg) != 0{
+            panic!("Failed mvwprint");
+        }
+        redraw(self.status_window);
+    }
+
 }
 
 fn fe_shut(){
@@ -279,56 +290,9 @@ fn fe_shut(){
 }
 
 
-fn display_model(win:WINDOW, msg:&String) {
-    mvwprintw(win, 0,0,msg.as_str());
-    redraw(win);
-    wgetch(win);
-}
-
-fn make_menu(win:WINDOW, menu_items:&Vec<(&String, usize)>, title:&str) -> usize{
-
-    // Return index of item selected.  Not bounded so caller must
-    // check
-
-    werase(win);
-
-    let (max_x, max_y) = (COLS(), LINES());
-    // Top left of main menu
-    let start_x = max_x/10 as i32;
-    let start_y = max_y/10 as i32;
-    
-    mvwprintw(win, start_y, start_x, title);
-    mvwprintw(win, start_y+1, start_x, "Enter choice");
-    for i in 0..menu_items.len() {
-        let item = format!("{} {}", i+1, &menu_items.iter().nth(i).unwrap().0);
-        mvwprintw(win, start_y+1+i as i32, start_x, &item);
-    }
-
-    // Add quit option
-    let y = start_y + menu_items.len() as i32 + 2;
-    mvwprintw(win, y, start_x, format!("{} {}", 0, "Back").as_str());
-    redraw(win);
-
-    let ret = wgetch(win);
-    if ret < 48 {
-        0
-    }else{
-        (ret - 48) as usize
-    }
-}
 
 
-fn status_line(msg:&String) {
-    let win = newwin(3, COLS(), LINES()-3, 0);    
-    box_(win, 0, 0);
-    redraw(win);
-    let msg = format!("{} {:?}", msg, env::current_dir());
-    if mvwprintw(win, 1, 1, &msg) != 0{
-        panic!("Failed mvwprint");
-    }
-    redraw(win);
-    //clrtoeol();
-}
+
 
 
 fn redraw(win:WINDOW) {
@@ -336,14 +300,3 @@ fn redraw(win:WINDOW) {
     wrefresh(win);
 }    
     
-fn main_window_dims() -> (i32, i32, i32, i32) {
-    (COLS()-4, LINES()-6, 2, 2)
-}
-
-
-fn destroy_win(win: WINDOW) {
-    let ch = ' ' as chtype;
-    wborder(win, ch, ch, ch, ch, ch, ch, ch, ch);
-    wrefresh(win);
-    delwin(win);
-}
