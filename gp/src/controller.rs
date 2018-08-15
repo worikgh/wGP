0076776822c11520bea66f01b0b22146bd9a03be
W <git@worik.org>
2018-08-15 20:32:53 +1200

    Analyse results set/get command

--- gp/src/controller.rs
+++ gp/src/controller.rs
@@ -6,16 +6,17 @@
 use config::Config;
 use config_default::ConfigDefault;
 use population::Population;
+use population::PopulationAnalysis;
 use std::collections::HashMap;    
 use std::collections::hash_map::Entry::Occupied;
 use std::collections::hash_map::Entry::Vacant;
-use std::fs::File;
-use std::path::Path;
+//use std::fs::File;
+//use std::path::Path;
 use std::sync::{Mutex, Arc};
 use std::thread;
 use std::time::Instant;
 
-#[derive(Clone)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum SimulationCommand {
     // Used to tell a running simulation to stop generating
     // generations and do some thing else
@@ -25,7 +26,10 @@ pub enum SimulationCommand {
 
     // Run Population::analyse to report on the quality of the
     // simulation
-    Analyse, 
+    Analyse,
+
+    // Run a simulation
+    Simulate,
 }
 
 pub struct Controller {
@@ -46,45 +50,68 @@ impl Controller {
             handles:HashMap::new(),
         }
     }
-    pub fn get_status(&mut self, proj_name:&str) -> SimulationStatusReport {
-        if let Occupied(entry)  = self.handles.entry(proj_name.to_string()) {
-            SimulationStatusReport::new(&(*entry.get().0.lock().unwrap()).copy(), &entry.get().2)
+    pub fn get_status(& self, proj_name:&str) -> SimulationStatus {
+        if let Some(ref entry)  = self.handles.get(proj_name) {
+            {
+                // entry is ref to a 3-tuple (Arc<SimulationStatus>, Handle, Config)
+                let arc = &entry.0;
+                arc.lock().unwrap().clone()
+            }
         }else{
-            SimulationStatusReport::new(&SimulationStatus::new(), &ConfigDefault::new(proj_name))
+            SimulationStatus::new(false, SimulationCommand::Empty)
         }
     }
     
-    pub fn run_simulation(&mut self, config: & Config) -> Result<usize, &str> {
-
-        // Run a simulation.  The config structure has all the
-        // information needed
-
-        
-        // Get the project configuration and over write the defaults.
-        // FIXME Should this be done here or in
-        // Controller::run_simulation?
-        let proj_dir = format!("{}/Data/", config.get_string("root_dir").unwrap());
-        let cfg_fname = format!("{}{}/.gp_config", proj_dir, config.get_string("name").unwrap());
-
-        let mut config = config.copy();
-        let path = Path::new(&cfg_fname);
-        if let Ok(f) = File::open(path) {
-            let _cfg = Config::new_file(f);
-            for key in _cfg.data.keys() {
-                // Over ride a default
-                let g = _cfg.get_string(&key).unwrap();
-                let v = g.clone();
-                config.data.insert(key.clone(), v);
-            }
+    pub fn get_command(&self, proj_name:&str) ->SimulationCommand {
+        if let Some(ref entry) = self.handles.get(proj_name) {
+            // entry is ref to a 3-tuple (Arc<SimulationStatus>, Handle, Config)
+            let arc = &entry.0;
+            arc.lock().unwrap().command.clone()
         }else{
-            eprintln!("Cannot find local config: {}", cfg_fname);
-        };
-        // Ensure simulation knows where to run
-        config.data.insert("proj_dir".to_string(), proj_dir);
+            SimulationCommand::Empty
+        }
+    }
+    pub fn set_command(&mut self, proj_name:&str, command:SimulationCommand)  {
+        eprintln!("set_command {:?}", command);
+        if !self.running(proj_name) {
+            eprintln!("set_command {:?} for {}", command, proj_name);
+            self._launch_thread(command, proj_name);
+        }else{
+            match self.handles.get(proj_name) {
+                Some(ref entry) => {
+                    eprintln!("seting_command {:?}", command);
+                    // entry is ref to a 3-tuple (Arc<SimulationStatus>, Handle, Config)
+                    let arc = &entry.0;
+                    arc.lock().unwrap().command = command;
+                },
+                None => panic!("Project {} says running but no thread handle available", proj_name),
+            };
+        }
+    }
+    fn _launch_thread(&mut self, command:SimulationCommand, name:&str) {
+
+        let config = ConfigDefault::new(name).specialise(name);        
+
+        // Create the shared memory to monitor and control simulation
+        let bb = Arc::new(Mutex::new(SimulationStatus::new(true, command)));
+
+        // FIXME When a simulation is not running anymore, where is
+        // the population?  In particular the Forest?
+        panic!("Fix this!");
+        let mut p = Population::new(&config);
         
-        // Check not already running
-        let name = config.get_string("name").unwrap();
-        let running = match self.handles.entry(name.to_string()) {
+        let h = p.run_in_thread(bb.clone());
+        self.handles.insert(String::from(name), (bb, h, config.copy()));
+        eprintln!("Set thread in motion");
+    }
+    
+    pub fn running(&mut self, name:&str) -> bool {
+        // Return true if there is a thread handle for this and it is
+        // still running
+
+        // FIXME?? This depends on the thread resetting `runnning` as
+        // it exits in Population::run_in_thread
+        match self.handles.entry(name.to_string()) {
             Occupied(o) => {
                 let o1 = o.get();
                 let ps = &*(o1.0).lock().unwrap();
@@ -98,45 +125,58 @@ impl Controller {
                 eprintln!("Not Running");
                 false
             }
-        };
-        if !running {
+        }
+    }
+        
+    pub fn run_simulation(&mut self, name:&str) -> Result<usize, &str> {
 
-            // Create the shared memory to monitor and control simulation
-            let bb = Arc::new(Mutex::new(SimulationStatus{running:false,
-                                                          cleared:true,
-                                                          start:Instant::now(),
-                                                          generation:0,
-                                                          command:SimulationCommand::Empty,
-                                                          path:config.get_string("proj_dir").unwrap(),
-            }));
-            let mut p = Population::new(&config);
-            let h = p.run_in_thread(bb.clone());
-            self.handles.insert(String::from(name.as_str()), (bb, h, config.copy()));
+        // Run a simulation.  The config structure has all the
+        // information needed
+        
+
+
+        // Check not already running
+        if !self.running(name) {
+            self._launch_thread(SimulationCommand::Simulate,  name);
             Ok(0)
         }else{
             Err("Running already")
         }
     }
-    pub fn get_config(&mut self, proj_name:&str) -> Option<Config> {
-        if let Occupied(entry)  = self.handles.entry(proj_name.to_string()) {
+    pub fn get_config(& self, proj_name:&str) -> Option<Config> {
+        if let Some(entry)  = self.handles.get(proj_name) {
             // FIXME If the simulation ever can write to Config object
             // this will need a lock
-            Some(entry.get().2.copy())
+            Some(entry.2.copy())
         }else{
             None
         }
     }
 }
 
+#[derive(Clone, Debug)]
+pub struct SimulationStatus {
+
+    // Front end unsets this to stop simulation gently
+    pub cleared:bool,
+    pub running:bool,
+    pub generation:usize,
+    pub start:Instant, // When started
+    pub command:SimulationCommand,
+    pub analysis:Option<PopulationAnalysis>,
+    pub population:usize,
+}
+
 impl SimulationStatus {
-    pub fn new() -> SimulationStatus{
+    pub fn new(cleared:bool, command:SimulationCommand) -> SimulationStatus{
         SimulationStatus{
             start:Instant::now(),
-            cleared:false,
+            cleared:cleared,
             running:false,
             generation:0,
-            path:"".to_string(),
-            command:SimulationCommand::Empty,
+            population:0,
+            command:command,
+            analysis:None,
         }
     }
     pub fn copy(&self) -> SimulationStatus{
@@ -145,46 +185,11 @@ impl SimulationStatus {
             cleared:self.cleared,
             running:self.running,
             generation:self.generation,
-            path:self.path.clone(),
+            population:self.population,
             command:self.command.clone(),
+            analysis:self.analysis.clone(),
         }
     }
 }
 
-pub struct SimulationStatus {
 
-    // Front end unsets this to stop simulation gently
-    pub cleared:bool,
-    pub running:bool,
-    pub generation:usize,
-    pub path:String, // FIXME This should be a reference
-    pub start:Instant, // When started
-    pub command:SimulationCommand,
-}
-
-impl SimulationStatusReport {
-    pub fn new(status:&SimulationStatus, config:&Config) -> SimulationStatusReport {
-        SimulationStatusReport {
-            cleared:status.cleared,
-            running:status.running,
-            generation:status.generation,
-            path:status.path.clone(),
-            started:status.start,
-            max_population:config.get_usize("max_population").unwrap(),
-            num_generations:config.get_usize("num_generations").unwrap(),
-            created:Instant::now(),
-        }
-    }
-    
-}
-
-pub struct SimulationStatusReport {
-    pub cleared:bool,
-    pub running:bool,
-    pub generation:usize,
-    pub path:String, // FIXME This should be a reference
-    pub created:Instant, // When this report created
-    pub started:Instant, // When simulation started
-    pub max_population:usize,
-    pub num_generations:usize,
-}
