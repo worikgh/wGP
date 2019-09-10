04f1cec047c6e6c481a1160af233ff0c0ba9038d
W <git@worik.org>
2019-09-11 09:24:52 +1200

    Nolonger a classifier

--- gp/src/score.rs
+++ gp/src/score.rs
@@ -1,10 +1,8 @@
 use super::NodeBox;
 use super::Data;
-//use data::Class;
 use inputs::Inputs;
 use std::cmp::Ordering;
-//use std;
-use super::rng;
+
 /// Scoring a individual is key to evolving a good population of
 /// individuals.
 
@@ -26,20 +24,17 @@ pub struct Score {
     // Fitness calculated when classifying to self.class.unwrap()
     pub quality:f64,
 
-    //  The name of the class this score is specialised for. Obtained
-    //  from the objective data
-    pub class:String,
 }
 
 impl Score {
 
-    pub fn evaluate(&self) -> f64 {
+    pub fn quality(&self) -> f64 {
         self.quality
     }
     
     pub fn partial_cmp(&self, other:&Score) -> Option<Ordering> {
         // For ordering array of scores
-        self.evaluate().partial_cmp(&other.evaluate())
+        self.quality().partial_cmp(&other.quality())
     }
     pub fn is_finite(&self) -> bool {
         self.quality.is_finite()
@@ -47,15 +42,14 @@ impl Score {
 
     #[allow(dead_code)]
     pub fn copy(&self) -> Score {
-        let class = self.class.clone();
-        Score{quality:self.quality, class:class}
+        Score{quality:self.quality, }
     }
 }
 
 impl Ord for Score {
     fn cmp(&self, other: &Score) -> Ordering {
-        let s1 = self.evaluate();
-        let s2 = other.evaluate();
+        let s1 = self.quality();
+        let s2 = other.quality();
         let o = s1 - s2;
         if o > 0.0 {
             Ordering::Less
@@ -90,11 +84,15 @@ fn loss_function(t:f64, y:f64) -> f64 {
         loss
     }
 }
-
+#[derive(Debug)]
+pub enum ScoreError {
+    FailedEvaluation,
+    NonFiniteSummation,
+}
 pub fn score_individual(
     node:&NodeBox,
     d:&Data,
-    use_testing:bool) -> Score {
+    use_testing:bool) -> Result<Score, ScoreError> {
 
     // Score individual is called once per node.  Classifies it
     // (decides what class it is for) and gives it a rating.
@@ -119,79 +117,46 @@ pub fn score_individual(
         index = &d.training_i;
     }
 
-    let mut c:Option<String> = None;
-    let mut best_s = 0.0;// std::f64::MIN;
-
     let mut inputs = Inputs::new();
 
-    for  class in d.class_names.iter() {
 
-        // Store each distance from the estimate to the actual value
-        // to calculate best and mean estimate
+    // Store each distance from the estimate to the actual value
+    // to calculate best and mean estimate
 
-        let mut y_d:Vec<f64> = Vec::new(); // Distances
+    let mut y_d:Vec<f64> = Vec::new(); // Distances
 
-        for i in index {
+    for i in index {
 
-            // Examine each example
+        // Examine each example
 
-            // The data for this example
-            let ref r = d.ith_row(*i);
+        // The data for this example
+        let ref r = d.ith_row(*i);
 
-            // Prepare the inputs to the simulation
-            for j in 0..d.input_names.len() {
-                let v:f64 = r[j];
-                let h = d.input_names[j].clone();
-                inputs.insert(h.as_str(), v);
-            }
-
-            // Is this example in the class of this node?  True value
-            // is -1.0 if this the input is not a member of the class
-            // the individual tests for, and 1.0 if it is for.
-
-            let t = if r[d.class_idx(class)] > 0.0 {
-                1.0
-            }else{
-                -1.0
-            };
-
-            // Get the estimate
-            let e = node.evaluate(&inputs).unwrap();
-            
-            let l = loss_function(t, e);
-            y_d.push(l);
+        // Prepare the inputs to the simulation
+        for j in 0..d.input_names.len() {
+            let v:f64 = r[j];
+            let h = d.input_names[j].clone();
+            inputs.insert(h.as_str(), v);
         }
 
-        let mut s = (1.0/(index.len() as f64))*
-            y_d.iter().fold(0.0, |mut sum, &x| {sum += x; sum});
-        if s.is_finite() {
-            // The score must finite and be increasing with quality.
-            // The above is a quantity to minimise.  Cannot use a
-            // direct inverse as a zero will cause a exception.  Zero
-            // is the floor of s
-            s = 1.0/(1.0+s);
-            
-            if s >= best_s {
-                c = Some(class.clone());
-                best_s = s;
-            }
-        }
+        let t = r[d.names.len()-1];
+        
+        // Get the estimate
+        match node.evaluate(&inputs) {
+            Some(e) => {        
+                let l = loss_function(t, e);
+                y_d.push(l);
+            },
+            None => return Err(ScoreError::FailedEvaluation),
+        };
     }
-    let _c = match c{
-        Some(c) => c,
-        None => {
-            // This only happens if there as no finite score for the
-            // Node for any class. 
-            let n = rng::gen_range(0, d.class_names.len());
-            d.class_names.iter().nth(n).unwrap().to_string()
-        }
-    };
 
-    //println!("Node: {} Score: {} Class: {}", node.to_string(), best_s, _c);
-    Score{quality:best_s,
-          // FIXME This should be a reference with a life time.  This
-          // string should be in population.class_names only
-          class:_c,
+    let s = (1.0/(index.len() as f64))*
+        y_d.iter().fold(0.0, |mut sum, &x| {sum += x; sum});
+    //println!("Node: {} Score: {}", node.to_string(), s);
+    match s.is_finite() {
+        true => Ok(Score{quality:s,}),
+        false => Err(ScoreError::NonFiniteSummation),
     }
 }
 
