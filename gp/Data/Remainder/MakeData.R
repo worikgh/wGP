1ad42be51c6657a2506b814f754a6065b04d64a3
W <git@worik.org>
2019-09-11 09:09:54 +1200

    Not a classifier any more

--- gp/Data/Remainder/MakeData.R
+++ gp/Data/Remainder/MakeData.R
@@ -1,6 +1,5 @@
 ## Calculate if input % 3 is 0, 1, or 2
 q <- sample(seq(1,10000), 10000)
-data <- cbind(q, q%%3==0, q%%3==1, q%%3==2)
-colnames(data) <- c("Q", "Zero", "One", "Two")
-data <- rbind(c(1,0,0,0), data)
+data <- cbind(q, q%%3)
+colnames(data) <- c("Q", "Object")
 write.csv(data, "data.in", row.names=FALSE, quote=FALSE)
